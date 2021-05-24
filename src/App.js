7ea00a979659cb82e5ae35c2db5692869d02dba3
--system <mabhishek.netha@gmail.com>
2021-05-24 15:08:01 +0530

    changed app text2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>
+          I was changed on feature
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         changed on feature branch
+          Learn React
         </a>
       </header>
     </div>
